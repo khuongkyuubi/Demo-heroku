6dc4af5caebdc420e9da4475e3036ad1ad8ce1f2
khuongkyuubi <khuong.kyuuib@gmail.com>
2022-07-12 15:44:37 +0700

    Update with db

--- dist/index.js
+++ dist/index.js
@@ -5,16 +5,23 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+const book_router_1 = __importDefault(require("./src/router/book.router"));
 const PORT = parseInt(process.env.PORT) || 4500;
 const app = (0, express_1.default)();
 app.set("view engine", "ejs");
 app.set('views', './src/views');
+const DB_URI = process.env.DB_URI || "";
+mongoose_1.default.connect(DB_URI)
+    .then(() => console.log('DB Connected!'))
+    .catch(error => console.log('DB connection error:', error.message));
 app.use(body_parser_1.default.json());
 app.get("/", ((req, res, next) => {
     return res.json({ message: "Hello World!" });
 }));
+app.use('/book', book_router_1.default);
 app.listen(PORT, () => {
     console.log("App running on port:", PORT);
 });
