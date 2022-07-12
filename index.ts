b48252f76523902f19f658bd761fbc1876b5a312
khuongkyuubi <khuong.kyuuib@gmail.com>
2022-07-12 14:48:32 +0700

    Update env

--- index.ts
+++ index.ts
@@ -6,18 +6,18 @@ dotenv.config();
 
 import bookRoutes from './src/router/book.router';
 
-const PORT = 3000;
+const PORT = parseInt(process.env.PORT) || 4500;
 const app = express();
 app.set("view engine", "ejs");
 app.set('views', './src/views');
 
-const DB_URL = `mongodb://localhost:27017/dbTest`;
-mongoose.connect(DB_URL)
+const DB_URI = process.env.DB_URI ||`mongodb://localhost:27017/dbTest`;
+mongoose.connect(DB_URI)
 .then(() => console.log('DB Connected!'))
 .catch(error => console.log('DB connection error:', error.message));
 app.use(bodyParser.json());
 
 app.use('/book', bookRoutes);
 app.listen(PORT, () => {
-  console.log("App running on port: " + PORT)
+  console.log("App running on port:", PORT)
 })
